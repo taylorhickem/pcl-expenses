c49d4f4d0d88020beae21698f7b58c862a3ded68
taylorhickem <taylor.hickem@gmail.com>
2023-07-08 13:37:06 +0800

    lambda function

--- moneymanager.py
+++ moneymanager.py
@@ -26,6 +26,7 @@ TABLES = {
 DATE_FORMAT = '%d-%m-%Y'
 CSV_ENCODING = 'iso-8859-1'
 CSV_FILENAME = '2022.csv'
+XLSX_FILENAME = '2023-01-01 ~ 12-31.xlsx'
 REPORING_YEAR = 2020
 REPORING_MONTH = 12
 
@@ -36,7 +37,8 @@ REPORING_MONTH = 12
 def update(txns=None):
     load()
     load_transactions(events=txns)
-    update_report()
+    post_to_gsheet()
+    print('report updated!')
 
 
 #-----------------------------------------------------------------------------
@@ -59,20 +61,22 @@ def update_config():
 #-----------------------------------------------------------------------------
 #subfunctions
 #-----------------------------------------------------------------------------
-def load_transactions(events=None):
+def load_transactions(events=None, event_format='xlsx'):
     global TABLES
     if events is None:
-        events = pd.read_csv(CSV_FILENAME, encoding=CSV_ENCODING)
+        if event_format == 'xlsx':
+            events = pd.read_excel(XLSX_FILENAME)
+        elif event_format == 'csv':
+            events = pd.read_csv(CSV_FILENAME, encoding=CSV_ENCODING)
     subfields = ['Period',
                  'Category',
                  'Subcategory',
                  'Amount']
-    events['start_date'] = events['Period'].apply(
-        lambda x: dt.datetime.strptime(x, DATE_FORMAT).date())
+    events['start_date'] = events['Period'].apply(lambda x: x.date())
     events['day'] = events['start_date'].apply(lambda x: x.day)
     events['month'] = events['start_date'].apply(lambda x: x.month)
     events['year'] = events['start_date'].apply(lambda x: x.year)
-    events = events[events['Income/Expenses'] == 'Expenses'].copy()
+    events = events[events['Income/Expense'] == 'Expense'].copy()
     events = events[events['year'] == REPORING_YEAR].copy()
     events = events[events['month'] <= REPORING_MONTH].copy()
     events_pvt = pd.pivot_table(events, index='Category', columns='month',
@@ -81,12 +85,25 @@ def load_transactions(events=None):
     TABLES['main_category_report'] = events_pvt
 
 
-def update_report():
+def post_to_gsheet():
+    # 01 main category
     main_category_report = TABLES['main_category_report']
+    # data fields
     db.post_to_gsheet(main_category_report, 'expenses', 'main_category_report',
                       input_option='USER_ENTERED')
+    # category field
     db.post_to_gsheet(main_category_report.reset_index()[['Category']],
                       'expenses', 'main_categories',
+                      input_option='USER_ENTERED')
+
+    # 02 subcategories
+    subcategory_report = TABLES['subcategory_report']
+    # data fields
+    db.post_to_gsheet(subcategory_report, 'expenses', 'subcategory_report',
+                      input_option='USER_ENTERED')
+    # category field
+    db.post_to_gsheet(subcategory_report.reset_index()[['Category', 'Subcategory']],
+                      'expenses', 'subcategories',
                       input_option='USER_ENTERED')
 
 
