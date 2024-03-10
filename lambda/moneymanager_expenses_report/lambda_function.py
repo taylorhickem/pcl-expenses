ff217c6b4c8a3954c1e11048808ddcb657d07be0
taylorhickem <taylor.hickem@gmail.com>
2024-03-10 16:46:46 +0800

    change transaction data file name

--- lambda/moneymanager_expenses_report/lambda_function.py
+++ lambda/moneymanager_expenses_report/lambda_function.py
@@ -7,7 +7,7 @@ import moneymanager as mm
 ENV_VARIABLES = [
     'S3_BUCKET'
 ]
-TXN_DATA_KEY = '2023-01-01 ~ 12-31.xlsx'
+TXN_DATA_KEY = 'txns.xlsx'
 USER_DATA_DIR = '/opt'
 user_data = {
     'gsheet_config': USER_DATA_DIR + '/gsheet_config.json',
