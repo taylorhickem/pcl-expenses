9a65bd13b3bf8500b04941411c34f48186dce454
taylorhickem <taylor.hickem@gmail.com>
2024-03-13 20:49:15 +0800

    refactor for dynamodb update

--- lambda/moneymanager_expenses_report/moneymanager.py
+++ lambda/moneymanager_expenses_report/moneymanager.py
@@ -1,19 +1,17 @@
 """ this script imports the export file from money manager app
-converts the raw file into two expense reports
+converts the raw file into an expense report with the tables:
 
+tables:
 1. main categories
-2. sub categories
 
-each report has rows of the categories (or subcategories)
+the report has rows of the categories
 and columns for each month in the year
 
 """
 #-----------------------------------------------------------------------------
 #import dependencies
 #-----------------------------------------------------------------------------
-import sys
 from sqlgsheet import database as db
-import datetime as dt
 import pandas as pd
 
 
@@ -21,29 +19,26 @@ import pandas as pd
 #Variables
 #-----------------------------------------------------------------------------
 TABLES = {
-    'events': []
+    'events': pd.DataFrame([])
 }
-DATE_FORMAT = '%d-%m-%Y'
-CSV_ENCODING = 'iso-8859-1'
-CSV_FILENAME = '../../2022.csv'
-XLSX_FILENAME = '../../2023-01-01 ~ 12-31.xlsx'
-REPORING_YEAR = 2020
-REPORING_MONTH = 12
+FIELDS = {
+    'amount': 'SGD',
+    'start_date': 'Period',
+    'txn_type': 'Income/Expense',
+    'category': 'Category'
+}
+TXN_TYPE_EXPENSE = 'Expense'
+REPORTING_YEAR = 0
+REPORTING_MONTH = 0
 
 
-#-----------------------------------------------------------------------------
-#main
-#-----------------------------------------------------------------------------
-def update(txns=None):
+def update(txns: pd.DataFrame):
     load()
-    load_transactions(events=txns)
+    load_transactions(txns)
     post_to_gsheet()
     print('report updated!')
 
 
-#-----------------------------------------------------------------------------
-#setup
-#-----------------------------------------------------------------------------
 def load():
     db.load_config()
     db.load_gsheet()
@@ -51,37 +46,25 @@ def load():
 
 
 def update_config():
-    global REPORING_YEAR, REPORING_MONTH
+    global REPORTING_MONTH, REPORTING_YEAR
     config = db.get_sheet('expenses', 'config')
     parameters = config[config['group'] == 'reporting'][
         ['parameter', 'value']].set_index('parameter')['value']
-    REPORING_YEAR = int(parameters['reporting_year'])
-    REPORING_MONTH = int(parameters['reporting_month'])
+    REPORTING_YEAR = int(parameters['reporting_year'])
+    REPORTING_MONTH = int(parameters['reporting_month'])
 
 
-#-----------------------------------------------------------------------------
-#subfunctions
-#-----------------------------------------------------------------------------
-def load_transactions(events=None, event_format='xlsx'):
+def load_transactions(events: pd.DataFrame):
     global TABLES
-    if events is None:
-        if event_format == 'xlsx':
-            events = pd.read_excel(XLSX_FILENAME)
-        elif event_format == 'csv':
-            events = pd.read_csv(CSV_FILENAME, encoding=CSV_ENCODING)
-    subfields = ['Period',
-                 'Category',
-                 'Subcategory',
-                 'Amount']
-    events['start_date'] = events['Period'].apply(lambda x: x.date())
+    events['start_date'] = events[FIELDS['start_date']].apply(lambda x: x.date())
     events['day'] = events['start_date'].apply(lambda x: x.day)
     events['month'] = events['start_date'].apply(lambda x: x.month)
     events['year'] = events['start_date'].apply(lambda x: x.year)
-    events = events[events['Income/Expense'] == 'Expense'].copy()
-    events = events[events['year'] == REPORING_YEAR].copy()
-    events = events[events['month'] <= REPORING_MONTH].copy()
-    events_pvt = pd.pivot_table(events, index='Category', columns='month',
-                             values='Amount', aggfunc='sum')
+    events = events[events[FIELDS['txn_type']] == TXN_TYPE_EXPENSE].copy()
+    events = events[events['year'] == REPORTING_YEAR].copy()
+    events = events[events['month'] <= REPORTING_MONTH].copy()
+    events_pvt = pd.pivot_table(events, index=FIELDS['category'], columns='month',
+        values=FIELDS['amount'], aggfunc='sum')
     events_pvt.fillna(0, inplace=True)
     TABLES['main_category_report'] = events_pvt
 
@@ -89,44 +72,9 @@ def load_transactions(events=None, event_format='xlsx'):
 def post_to_gsheet():
     # 01 main category
     main_category_report = TABLES['main_category_report']
-    # data fields
     db.post_to_gsheet(main_category_report, 'expenses', 'main_category_report',
                       input_option='USER_ENTERED')
-    # category field
     db.post_to_gsheet(main_category_report.reset_index()[['Category']],
                       'expenses', 'main_categories',
                       input_option='USER_ENTERED')
 
-    # 02 subcategories
-    #subcategory_report = TABLES['subcategory_report']
-
-    # data fields
-    #db.post_to_gsheet(subcategory_report, 'expenses', 'subcategory_report',
-    #                  input_option='USER_ENTERED')
-    # category field
-    #db.post_to_gsheet(subcategory_report.reset_index()[['Category', 'Subcategory']],
-    #                  'expenses', 'subcategories',
-    #                  input_option='USER_ENTERED')
-
-
-# -----------------------------------------------------
-# Command line interface
-# -----------------------------------------------------
-def autorun():
-    if len(sys.argv) > 1:
-        process_name = sys.argv[1]
-        if process_name == 'pink_floyd':
-            print('dont take a slice of my pie')
-    else:
-        update()
-
-
-if __name__ == "__main__":
-    autorun()
-# -----------------------------------------------------
-# Reference code
-# -----------------------------------------------------
-
-#-----------------------------------------------------------------------------
-#main
-#-----------------------------------------------------------------------------
